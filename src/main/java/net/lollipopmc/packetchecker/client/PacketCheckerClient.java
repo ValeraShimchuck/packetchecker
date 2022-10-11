7b552cf94498279ce16b0e8cd6a616232e9de3da
ValeraShimchuck <valera01092006@gmail.com>
2022-10-11 15:30:16 +0300

    save

--- src/main/java/net/lollipopmc/packetchecker/client/PacketCheckerClient.java
+++ src/main/java/net/lollipopmc/packetchecker/client/PacketCheckerClient.java
@@ -6,10 +6,14 @@ import net.fabricmc.api.Environment;
 import net.minecraft.item.Item;
 import net.minecraft.item.Items;
 import net.minecraft.network.ClientConnection;
+import net.minecraft.network.NetworkSide;
+import net.minecraft.network.NetworkState;
+import net.minecraft.network.packet.s2c.play.MapUpdateS2CPacket;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Field;
+import java.util.HexFormat;
 
 @Environment(EnvType.CLIENT)
 public class PacketCheckerClient implements ClientModInitializer {
@@ -18,7 +22,8 @@ public class PacketCheckerClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        LOGGER.info(Item.getRawId(Items.PLAYER_HEAD) + "");
+        LOGGER.info(NetworkState.PLAY.getPacketId(NetworkSide.CLIENTBOUND, new MapUpdateS2CPacket(0, (byte) 0, false, null, null)) + "");
+
     }
 
 }
