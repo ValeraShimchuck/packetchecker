21ccb04e4356dd3fbdbca7a5c274ef304ea4470a
ValeraShimchuck <valera01092006@gmail.com>
2023-04-19 21:03:36 +0300

    commit

--- src/main/java/net/lollipopmc/packetchecker/mixin/PacketCheckerMixin.java
+++ src/main/java/net/lollipopmc/packetchecker/mixin/PacketCheckerMixin.java
@@ -5,6 +5,7 @@ import io.netty.channel.ChannelHandlerContext;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.network.*;
 import net.minecraft.network.listener.PacketListener;
+import net.minecraft.network.packet.Packet;
 import net.minecraft.network.packet.c2s.handshake.HandshakeC2SPacket;
 import net.minecraft.network.packet.c2s.login.LoginHelloC2SPacket;
 import net.minecraft.network.packet.c2s.login.LoginKeyC2SPacket;
@@ -47,17 +48,17 @@ public class PacketCheckerMixin {
         if (packet instanceof LightUpdateS2CPacket) return;
         //if (packet instanceof EntityTrackerUpdateS2CPacket) return;
         LOGGER.info("from server: " + SERVER_PACKET_MAPPINGS.getOrDefault(packet.getClass(), packet.getClass().toString()));
-        if (packet instanceof QueryResponseS2CPacket responseS2CPacket) {
-            Gson gson = new Gson();
-            LOGGER.info(gson.toJson(responseS2CPacket.getServerMetadata()));
-        }
-        if (packet instanceof ChunkDataS2CPacket chunkDataS2CPacket) {
-            NbtCompound nbt = chunkDataS2CPacket.getChunkData().getHeightmap();
-            System.out.println(nbt.toString());
-        }
-        if (packet instanceof LoginSuccessS2CPacket loginSuccessS2CPacket) {
-            System.out.println("login: " + loginSuccessS2CPacket.getProfile());
-        }
+        //if (packet instanceof QueryResponseS2CPacket responseS2CPacket) {
+        //    Gson gson = new Gson();
+        //    LOGGER.info(gson.toJson(responseS2CPacket.getServerMetadata()));
+        //}
+        //if (packet instanceof ChunkDataS2CPacket chunkDataS2CPacket) {
+        //    NbtCompound nbt = chunkDataS2CPacket.getChunkData().getHeightmap();
+        //    System.out.println(nbt.toString());
+        //}
+        //if (packet instanceof LoginSuccessS2CPacket loginSuccessS2CPacket) {
+        //    System.out.println("login: " + loginSuccessS2CPacket.getProfile());
+        //}
         //if (packet instanceof GameJoinS2CPacket gameJoinS2CPacket) {
         //    Codec<DynamicRegistryManager> codec = SerializableRegistries.CODEC;
         //    DynamicRegistryManager.Immutable data = gameJoinS2CPacket.registryManager();
@@ -75,7 +76,7 @@ public class PacketCheckerMixin {
         //context.pipeline().addFirst(new DuplexPacketListener());
     }
 
-    @Inject(method = "send(Lnet/minecraft/network/Packet;Lnet/minecraft/network/PacketCallbacks;)V", at = @At("HEAD"))
+    @Inject(method = "send(Lnet/minecraft/network/packet/Packet;Lnet/minecraft/network/PacketCallbacks;)V", at = @At("HEAD"))
     private void send(Packet<?> packet, PacketCallbacks callbacks, CallbackInfo ci) {
         if (packet instanceof PlayerMoveC2SPacket) return;
         LOGGER.info("to server: " + CLIENT_PACKET_MAPPINGS.getOrDefault(packet.getClass(), packet.getClass().toString()));
